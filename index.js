303730649c594de5c555af832bf81258de882985
stoleS <white.knight0857@gmail.com>
2018-09-19 02:15:20 +0200

    Started with app redesign and code revamp

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+// Define app
+const app = express();
+
+// Define socket
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res, next) => {
+  res.sendFile("index.html");
+});
+
+io.on("connection", socket => {
+  console.log(`${socket.id} connected`);
+});
+
+// Error catcher
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// Handle errors
+app.use((err, req, res, next) => {
+  const error = app.get("end") === "development" ? err : {};
+  const status = err.status || 500;
+
+  // After that respond to client
+  res.status(status).json({
+    error: {
+      message: error.message
+    }
+  });
+
+  // Push to the terminal
+  console.error(err);
+});
+
+// Define port and start the server
+const port = process.env.PORT || 4000;
+http.listen(port, () => console.log(`Server started on port ${port}`));

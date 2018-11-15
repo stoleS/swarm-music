db3d2af5a854433f1d0883d391d858f10e82079e
stoleS <white.knight0857@gmail.com>
2018-11-15 17:11:43 +0100

    Refreshed app

--- public/js/background.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("mousemove", e => {
-  const moveX = (window.innerWidth / 2 - e.pageX) * 0.05;
-  const moveY = (window.innerWidth / 2 - e.pageY) * 0.05;
-  const bg = document.getElementById("bg");
-  bg.style.marginLeft = `${moveX}px`;
-  bg.style.marginTop = `${moveY}px`;
-});
